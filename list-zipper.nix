e6c4b82c63cae442cb7ee7cb5fee504ae5a2a910
Brian McKenna <brian@brianmckenna.org>
2023-10-25 20:06:51 +1100

    Regen list-zipper.nix

--- list-zipper.nix
+++ list-zipper.nix
@@ -1,21 +1,21 @@
 { mkDerivation, base, checkers, comonad, deriving-compat, hedgehog
-, hedgehog-fn, lens, mtl, QuickCheck, semigroupoids, semigroups
-, stdenv, tasty, tasty-hedgehog, tasty-hunit, tasty-quickcheck
-, transformers
+, hedgehog-fn, lens, lib, MonadRandom, mtl, QuickCheck
+, random-shuffle, semigroupoids, semigroups, tasty, tasty-hedgehog
+, tasty-hunit, tasty-quickcheck, transformers
 }:
 mkDerivation {
   pname = "list-zipper";
-  version = "0.0.9";
+  version = "0.0.11";
   src = ./.;
   libraryHaskellDepends = [
-    base comonad deriving-compat lens mtl semigroupoids semigroups
-    transformers
+    base comonad deriving-compat lens MonadRandom mtl random-shuffle
+    semigroupoids semigroups transformers
   ];
   testHaskellDepends = [
     base checkers hedgehog hedgehog-fn lens mtl QuickCheck tasty
     tasty-hedgehog tasty-hunit tasty-quickcheck transformers
   ];
-  homepage = "https://github.com/qfpl/list-zipper";
+  homepage = "https://github.com/system-f/list-zipper";
   description = "A list zipper";
-  license = stdenv.lib.licenses.bsd3;
+  license = lib.licenses.bsd3;
 }
